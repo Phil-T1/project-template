c96e59e6ddb087382f7245d94cb1c61862f7dffb
Phil-T1 <88270060+Phil-T1@users.noreply.github.com>
2023-09-28 20:35:33 +0100

    Update setup.bat

--- setup/setup.bat
+++ setup/setup.bat
@@ -5,11 +5,8 @@ REM Configures Python setup for project
 REM Get the directory of the batch script
 SET "SCRIPT_DIR=%~dp0"
 
-REM Set project ROOT_DIR to the parent directory of the script
-SET "ROOT_DIR=%SCRIPT_DIR:~0,-1%"
-
-ECHO Switching to project root dir: %ROOT_DIR%
-CD /D "%ROOT_DIR%"
+REM Switch to batch dir, then to project root
+CD %SCRIPT_DIR%\..
 
 REM Check if venv exists
 IF EXIST "venv" (
@@ -28,6 +25,6 @@ IF EXIST "venv" (
 )
 
 ECHO Installing packages into virtual environment...
-START CMD /k "%ROOT_DIR%\venv\Scripts\activate.bat && pip install -r %ROOT_DIR%\requirements.txt && pre-commit install"
+START CMD /k ".\venv\Scripts\activate.bat && pip install -r .\requirements.txt && pre-commit install"
 
 PAUSE
