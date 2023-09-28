58815027df0f5d4e15276b27c54bb7299a32593b
Phil Tegg <phil.tegg@gmail.com>
2023-09-28 13:55:34 +0100

    Test.

--- src/blob.py
+++
@@ -1,6 +0,0 @@
-"""This module is a simple test that returns a text sring."""
-
-
-def blob(blob: str) -> str:
-    """Return a blob."""
-    return f"blob{blob}blobblobblobblob"
--- src/counter.py
+++
@@ -1,9 +0,0 @@
-"""This module is a simple test that returns a list of numbers."""
-
-
-def counter(num: int) -> int:
-    """Count in ones from num a to num b."""
-    list_counter = []
-    for i in range(1, num + 1):
-        list_counter.append(i)
-    return list_counter
